e632fb84981e27dff6ef8d3e84d5cea28041291e
mayurk17 <mayurk1703@gmail.com>
2025-10-09 17:32:09 +0530

    Create data_segmentation.sql

---
+++ scripts/data_segmentation.sql
@@ -0,0 +1,31 @@
+/*
+
+Data Segmentation Analysis
+
+Purpose:
+    - To group data into meaningful categories for targeted insights.
+    - For customer segmentation, product categorization, or regional analysis.
+*/
+
+/*Segment products into cost ranges and 
+count how many products fall into each segment*/
+
+WITH product_segments AS (
+    SELECT
+        product_key,
+        product_name,
+        cost,
+        CASE 
+            WHEN cost < 100 THEN 'Below 100'
+            WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+            WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+            ELSE 'Above 1000'
+        END AS cost_range
+    FROM gold.dim_products
+)
+SELECT 
+    cost_range,
+    COUNT(product_key) AS total_products
+FROM product_segments
+GROUP BY cost_range
+ORDER BY total_products DESC;
